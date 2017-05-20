0e8cb897f3af83699e821c8e927fa3fa8c09bf7e
Mani <tmanikandan05@gmail.com>
2017-05-20 13:32:48 +0530

    First commit

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  entry: 'src/scripts/main.js',
+  dest: 'build/js/main.min.js',
+  format: 'iife',
+  sourceMap: 'inline',
+  plugins: [
+       babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      presets: [ [ 'es2015', { modules: false } ], 'react' ],
+      plugins: [ 'external-helpers' ]
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+      replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
+};
